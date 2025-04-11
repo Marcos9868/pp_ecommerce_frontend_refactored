938b4e08b47de3fd2372384eb2ba46dfc1f618ab
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-04-11 15:47:36 -0300

    Feat: Product Grid
    Add a grid of product on page

---
+++ src/components/productGrid/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ProductCard } from "../productCard";
+import { IProduct } from "../productCard/types";
+import { ProductGridProps } from "./props";
+
+export const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
+  return (
+    <div
+      className="grid grid-cols-1 sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-[repeat(auto-fit, minmax(250px, 1fr))]
+      gap-6
+      p-4"
+    >
+      {products.map((product: IProduct) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
